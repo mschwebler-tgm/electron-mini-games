4e33150bb67eec3d526eef828c61e90ce67b99ec
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2020-04-15 13:28:56 +0200

    Shuffles tiles on creation.

--- tests/unit/memory.spec.ts
+++ tests/unit/memory.spec.ts
@@ -9,4 +9,13 @@ describe('Memory', () => {
 
         expect(memoryTiles.length).toBe(subjects.length * 2);
     });
+
+    it('should shuffle tiles on create', () => {
+        const subjects = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+        const memory = new Memory(subjects);
+
+        const memoryTiles = memory.getTiles();
+
+        expect(memoryTiles[0]).not.toBe(memoryTiles[1]);
+    });
 });
