fc550257c66a9d2e16dbd3b1d5648691a37a8732
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2020-04-20 13:13:57 +0200

    Animates stars on point gain.

--- src/components/games/memory/MemoryPoints.vue
+++ src/components/games/memory/MemoryPoints.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-        <template v-for="star in fullStars">
-            <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="48"
-                    width="48"
-                    viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-                      fill="orange"></path>
-            </svg>
-        </template>
+        <transition-group name="star-gain">
+            <template v-for="(star, index) in fullStars">
+                <svg
+                        :key="index"
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="48"
+                        width="48"
+                        viewBox="0 0 24 24">
+                    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+                          fill="orange"></path>
+                </svg>
+            </template>
+        </transition-group>
 
         <template v-for="star in emptyStars">
             <svg xmlns="http://www.w3.org/2000/svg"
@@ -22,6 +25,7 @@
 </template>
 
 <script>
+    import MemoryTile from "@/components/games/memory/MemoryTile";
     const MAX_STARS = 10;
 
     export default {
@@ -34,7 +38,7 @@
         },
         computed: {
             fullStars() {
-                return Math.min(MAX_STARS, Math.floor(this.points / 100));
+                return Math.min(MAX_STARS, Math.floor(this.points / (MemoryTile.FULL_POINTS * 1.5)));
             },
             emptyStars() {
                 return MAX_STARS - this.fullStars;
@@ -42,3 +46,36 @@
         },
     };
 </script>
+
+<style>
+    .star-gain-enter-active {
+        -webkit-animation: star-gain-animation .5s ease-out;
+        -o-animation: star-gain-animation .5s ease-out;
+        animation: star-gain-animation .5s ease-out;
+    }
+
+    @keyframes star-gain-animation {
+        0% {
+            -webkit-transform: scale3d(0, 0, 0);
+            -moz-transform: scale3d(0, 0, 0);
+            -ms-transform: scale3d(0, 0, 0);
+            -o-transform: scale3d(0, 0, 0);
+            transform: scale3d(0, 0, 0);
+        }
+        50% {
+            -webkit-transform: scale3d(1.2, 1.2, 1.2);
+            -moz-transform: scale3d(1.2, 1.2, 1.2);
+            -ms-transform: scale3d(1.2, 1.2, 1.2);
+            -o-transform: scale3d(1.2, 1.2, 1.2);
+            transform: scale3d(1.2, 1.2, 1.2);
+        }
+        100% {
+            -webkit-transform: scale3d(1, 1, 1);
+            -moz-transform: scale3d(1, 1, 1);
+            -ms-transform: scale3d(1, 1, 1);
+            -o-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+        }
+    }
+
+</style>
